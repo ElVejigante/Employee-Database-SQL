afda6030e437ef13f482d22c142eb95f5d601d9c
ElVejigante <ernesto.cancel@gmail.com>
2021-04-04 22:35:58 -0400

    dept_manager imported

--- EmployeeSQL/table_schema.sql
+++ EmployeeSQL/table_schema.sql
@@ -1,5 +1,5 @@
 --Table Schema
---Order: departments, titles, employees, dept_manager, dept_emp, employees, salaries
+--Order: departments, titles, employees, dept_manager, dept_emp, salaries
 --departments
 DROP TABLE departments;
 
@@ -25,10 +25,29 @@ DROP TABLE employees;
 
 CREATE TABLE employees (
 	emp_no INT PRIMARY KEY,
-	emp_title_id CHAR(5) FOREIGN KEY,
+	emp_title_id CHAR(5) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date DATE NOT NULL,
 	first_name VARCHAR NOT NULL,
 	last_name VARCHAR NOT NULL,
 	sex CHAR(1) NOT NULL,
 	hire_date DATE
 );
+
+SELECT * FROM employees;
+
+--dept_manager
+DROP TABLE dept_manager;
+
+CREATE TABLE dept_manager (
+	dept_no CHAR(4),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	emp_no INT,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM dept_manager;
+
+--dept_emp
+
+--salaries
