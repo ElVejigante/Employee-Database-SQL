8e56a798941c80cb02b6e4ba77727d086ed9f64a
ElVejigante <ernesto.cancel@gmail.com>
2021-04-05 00:16:05 -0400

    last query complete

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -56,3 +56,8 @@ WHERE departments.dept_name IN ('Sales', 'Development')
 ORDER BY emp_no
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(*) AS freq_count
+FROM employees
+GROUP BY last_name
+ORDER BY freq_count DESC
